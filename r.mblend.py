5efaf9126e9ae03e536bc4a99c506f8fa2b66414
Luís de Sousa <luis.de.sousa@protonmail.ch>
2017-04-20 11:53:56 +0200

    Adding identifier creation and temp map removal functions.

--- r.mblend.py
+++ r.mblend.py
@@ -14,6 +14,21 @@
 
 import grass.script as gscript
 
+index = 0
+TMP_MAPS = []
+
+
+def getTemporaryIdentifier():
+    id = 'tmp_raster_' + str(os.getpid()) + str(index)
+    index = index + 1
+    TMP_MAPS.append(id)
+    return id
+
+
+def cleanup():
+    gscript.run_command('g.remove', type='raster', name=','.join(TMP_MAPS), flags='f')
+
+
 def main():
     options, flags = gscript.parser()
     high = options['high']
@@ -24,7 +39,9 @@ def main():
     gscript.run_command('g.region', raster=high + "," + low)
     print gscript.region()
 
+
 if __name__ == '__main__':
     gscript.use_temp_region()
+	atexit.register(cleanup)
     main()
 
