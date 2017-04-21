b9dd1aca45200e38a9d0e98e1ca0ac2f900d75d7
Luís de Sousa <luis.de.sousa@protonmail.ch>
2017-04-21 10:58:18 +0200

    Adding commands to obtain area to interpolate.

--- r.mblend.py
+++ r.mblend.py
@@ -12,6 +12,8 @@
 #% key: low
 #%end
 
+import os
+import atexit
 import grass.script as gscript
 
 index = 0
@@ -19,6 +21,7 @@ TMP_MAPS = []
 
 
 def getTemporaryIdentifier():
+    global index
     id = 'tmp_raster_' + str(os.getpid()) + str(index)
     index = index + 1
     TMP_MAPS.append(id)
@@ -39,9 +42,31 @@ def main():
     gscript.run_command('g.region', raster=high + "," + low)
     print gscript.region()
 
+    # Make cell size compatible
+    low_res_inter = getTemporaryIdentifier()
+    gscript.run_command('r.resamp.interp', input=low, output=low_res_inter, method='nearest')
+
+	# Vectorise rasters
+    high_vect = getTemporaryIdentifier()
+    low_vect = getTemporaryIdentifier()
+    gscript.run_command('r.to.vect', input=high, output=high_vect, type='area')
+    gscript.run_command('r.to.vect', input=low, output=low_vect, type='area')
+
+    # Dissolve and overlay to obtain extent to interpolate
+    low_extent = getTemporaryIdentifier()
+    high_extent = getTemporaryIdentifier()
+    interpol_area = getTemporaryIdentifier()
+    gscript.run_command('v.db.addcolumn', map=low_vect, columns="flag integer")
+    gscript.run_command('v.db.update', map=low_vect, col="flag", qcol="1")
+    gscript.run_command('v.dissolve', input=low_vect, output=low_extent, column="flag")
+    gscript.run_command('v.db.addcolumn', map=high_vect, columns="flag integer")
+    gscript.run_command('v.db.update', map=high_vect, col="flag", qcol="1")
+    gscript.run_command('v.dissolve', input=high_vect, output=high_extent, column="flag")
+    gscript.run_command('v.overlay', ainput=low_extent, binput=high_extent, output=interpol_area, operator='not')
+
 
 if __name__ == '__main__':
+    atexit.register(cleanup)
     gscript.use_temp_region()
-	atexit.register(cleanup)
     main()
 
