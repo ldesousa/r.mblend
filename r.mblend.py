5705be5c02016e983c0fe2c73682394bcb3f43db
Luís de Sousa <luis.de.sousa@protonmail.ch>
2017-04-21 11:09:30 +0200

    Added commands to obtain edge points of high resolution.

--- r.mblend.py
+++ r.mblend.py
@@ -22,6 +22,7 @@ TMP_MAPS = []
 
 def getTemporaryIdentifier():
     global index
+    global TMP_MAPS
     id = 'tmp_raster_' + str(os.getpid()) + str(index)
     index = index + 1
     TMP_MAPS.append(id)
@@ -29,6 +30,8 @@ def getTemporaryIdentifier():
 
 
 def cleanup():
+    print("Temporary raster to remove:")
+    print(TMP_MAPS)
     gscript.run_command('g.remove', type='raster', name=','.join(TMP_MAPS), flags='f')
 
 
@@ -63,6 +66,20 @@ def main():
     gscript.run_command('v.db.update', map=high_vect, col="flag", qcol="1")
     gscript.run_command('v.dissolve', input=high_vect, output=high_extent, column="flag")
     gscript.run_command('v.overlay', ainput=low_extent, binput=high_extent, output=interpol_area, operator='not')
+
+	# Compute difference between the two rasters and vectorise to points
+    diff = getTemporaryIdentifier()
+    diff_points = getTemporaryIdentifier()
+    gscript.mapcalc(diff + " = highRes - lowResInter")
+    gscript.run_command('r.to.vect', input=diff, output=diff_points, type='point')
+
+	# Obtain edge points of the high resolution raster
+    interpol_area_buff = getTemporaryIdentifier()
+    diff_points_edge = getTemporaryIdentifier()
+	# 1. buffer around area of interest - pixel size must be known
+    gscript.run_command('v.buffer', input=interpol_area, output=interpol_area_buff, type='area', distance=20)
+	# 2. get the points along the edge
+    gscript.run_command('v.select', ainput=diff_points, binput=interpol_area_buff, output=diff_points_edge, operator='overlap')
 
 
 if __name__ == '__main__':
